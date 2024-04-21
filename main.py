1e5156d4dd3d5f08faf95d3ec4b7d4515c9159c0
apaww <64725988+apaww@users.noreply.github.com>
2024-04-21 20:17:45 +0300

    added presentation

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ client.remove_command('help')
 @client.event
 async def on_ready():
     await load_cogs()
-    print('ready, steady, go!')
+    print('Бот запустился!')
     await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=status))
 
 
